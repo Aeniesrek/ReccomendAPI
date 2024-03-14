9e0e8880cbc16b70702d6d995ff9fa945350cc72
Aeniesrek <47981820+Aeniesrek@users.noreply.github.com>
2024-03-15 03:41:57 +0900

    test

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ from fastapi import FastAPI
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from google.cloud import secretmanager
+import json
 
 app = FastAPI()
 
 @app.get("/")
 async def index():
     # 環境変数からJSONファイルのパスを取得する
-    json_file_path = get_secret("GOOGLE_APPLICATION_CREDENTIALS")
+    json_credentials = get_secret("GOOGLE_APPLICATION_CREDENTIALS")
     spread_sheet_key = get_secret("SPREADSHEET_KEY")
-    if not json_file_path or not spread_sheet_key:
+    if not json_credentials or not spread_sheet_key:
         return {"error": "Missing environment variables"}
 
-    ws = connect_gspread(json_file_path, spread_sheet_key)
+    credentials_dict = json.loads(json_credentials)
+    ws = connect_gspread(credentials_dict, spread_sheet_key)
     ds = ws.range('A1:G3')
     # ここでの返却値を適切な形式に変換する必要があるかもしれません
     return {"data": [cell.value for cell in ds]}
 
-def connect_gspread(json_file_path, key):
+def connect_gspread(credentials_dict, key):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_path, scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
     gc = gspread.authorize(credentials)
     worksheet = gc.open_by_key(key).sheet1
     return worksheet
@@ -32,7 +34,7 @@ def get_secret(key):
         return access_secret_version('hellowebapi-415301', key)
     else:
         # ローカル環境用の処理
-        return os.getenv(key, "")
+        return os.getenv(key,"")
 
 def access_secret_version(project_id, secret_id, version_id="latest"):
     """
